b87d41c15655beba2d592851fb4994f65866dc89
xiaoxiong huang <xiaoxiong.huang@tufts.edu>
2022-03-30 17:11:10 -0400

    sudoku solver

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# SUDOKU-SOLVER-
+# Homework 4 for CS131
+Sudoku Solver
+Implemented using Backtracking algorithm.
+
+## Usage
+```bash
+#   Run the program:
+python3 Sudoku_Solver.py
+```
+
+## Filtering
+I used forward checking to eliminate any invaild value.
+
+## Detail
+Detail about the code can be found in comment. Both simple and hard sudoku are hard coded, program will
+run simple and hard one next to each other. Also, I am assuming given sudoku is vaild(i.e. grid is 
+9X9 and only 1-9 is in sudoku and no law of sudoku is broken.)
--- Sudoku_Solver.py
+++ Sudoku_Solver.py
@@ -1,10 +1,14 @@
 import time
 
 class Sudoku:
+
     def __init__(self, sudoku: list[list[int]]) -> None:
+        #. sudoku board, list of lists.
         self.sudoku = sudoku
+        #. coordintes(row,col) for empty block
         self.emptyCoordintes = self.find_empty_space()
 
+    #. useful method to print out the sudoku
     def __str__(self) -> str:
         sudoku = "|---------|---------|---------|\n"
         for i in range(9):
@@ -25,58 +29,84 @@ class Sudoku:
             
         return sudoku
 
-
+    #. this method will find the possible number that can be put on the empty block
+    #. repersent by row and col.
     def find_possible_value(self, row: int, col: int) -> list[int]:
         if(self.sudoku[row][col] != 0):
             return None
+        # all possible number for a sudoku
         possibleList: list[int] = [1,2,3,4,5,6,7,8,9]
+        # check for every row
         for i in range(9):
             if(self.sudoku[row][i] == 0):
                 continue
+            # if some block have value not 0 and in possible list, remove it
             if(self.sudoku[row][i] in possibleList):
                 possibleList.remove(self.sudoku[row][i])
+        # check for every col
         for i in range(9):
             if(self.sudoku[i][col] == 0):
                 continue
-            if(self.sudoku[row][i] in possibleList):
-                possibleList.remove(self.sudoku[col][i])
+            # if some block have value not 0 and in possible list, remove it
+            if(self.sudoku[i][col] in possibleList):
+                possibleList.remove(self.sudoku[i][col])
+        # off set the row and col to left top block
         offsetRow = row - (row % 3)
         offsetCol = col - (col % 3)
+        # for every block in 3X3 square
         for i in range(3):
             for j in range(3):
                 existVar = self.sudoku[offsetRow + i][offsetCol + j]
+                # if some block have value in possible list, remove it
                 if(existVar in possibleList):
                     possibleList.remove(existVar)
+        # remaining value in the list is all possible value can be write to that block.
         return possibleList
     
+    # find all empty block in a sudoku, return result is list of (row,col)
     def find_empty_space(self) -> list[(int,int)]:
         result: list[(int,int)] = []
+        # go through every row and col
         for i in range(9):
             for j in range(9):
                 if(self.sudoku[i][j] == 0):
                     result.append((i,j))
         return result
 
-
+    # solve the sudoku recursivily
     def solve(self, count) -> str:
+        # if we have filled every empty block in a sudoku,
+        # we found the solution, so we stop recursive and return SUCC
         if(count == len(self.emptyCoordintes)):
             return "SUCC"
         
+        # get the corrdination for a empty block from the empty block list,
+        # see more in __init__ about emptyCoordintes[].
         corrdination = self.emptyCoordintes[count]
         row = corrdination[0]
         col = corrdination[1]
 
+        # find the all possible value we can put in this empty block,
+        # used function find_possible_value().
         possible_value: list[int] = self.find_possible_value(row, col)
+
+        # recursivily try all value in possible list.
         for value in possible_value:
             self.sudoku[row][col] = value
 
+            # increase the count by 1, means we are moving on to next empty block.
             if self.solve(count+1) == "SUCC":
                 return "SUCC"
             
+            # in case blow code will run, that means some block later on do not have any possible
+            # value that can be put on that block so return FAIL.
+            # we want to reset this block to 0 and try another value.
             self.sudoku[row][col] = 0
+        
         return "FAIL"
 
 def main():
+    
     simpleSudoku = [[6,0,8,7,0,2,1,0,0],
                     [4,0,0,0,1,0,0,0,2],
                     [0,2,5,4,0,0,0,0,0],
@@ -87,9 +117,35 @@ def main():
                     [2,0,0,0,9,0,0,0,8],
                     [0,0,6,8,0,5,2,0,3]]
     simpleS = Sudoku(simpleSudoku)
+    print("Sudoku before solve:")
     print(simpleS)
-    print(simpleS.solve(0))
+    start = time.time()
+    # solve the sudoku in place.
+    print("Outcome from solve the sudoku: " + simpleS.solve(0))
+    end = time.time()
+    print("Sudoku after solve:")
     print(simpleS)
+    print("Time used to solve the simple Sudoku: " + str(end-start))
+
+    harderSudoku = [[0,7,0,0,4,2,0,0,0],
+                    [0,0,0,0,0,8,6,1,0],
+                    [3,9,0,0,0,0,0,0,7],
+                    [0,0,0,0,0,4,0,0,9],
+                    [0,0,3,0,0,0,7,0,0],
+                    [5,0,0,1,0,0,0,0,0],
+                    [8,0,0,0,0,0,0,7,6],
+                    [0,5,4,8,0,0,0,0,0],
+                    [0,0,0,6,1,0,0,5,0]]
+    harderS = Sudoku(harderSudoku)
+    print("Sudoku before solve:")
+    print(harderS)
+    start = time.time()
+    print("Outcome from solve the sudoku: " + harderS.solve(0))
+    end = time.time()
+    print("Sudoku after solve:")
+    print(harderS)
+    print("Time used to solve the simple Sudoku: " + str(end-start))
+
 
 
 if __name__ == '__main__':
